1fa28dea5dd4a0b70eeba2e8a4de52b4cd2033e9
Harshitha sridhar <115911183+harshithaasridhar@users.noreply.github.com>
2023-07-17 12:28:54 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class conditions {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);{
+        int age = sc.nextInt();
+        if(age>=18){    
+            System.out.println("you can vote bruhh");
+        }else{
+            System.out.println("ahh plz go watch tv");
+        }
+
+    }
+    
+}
+}
