f2c3d9aa5f6b99749b79c73da54bc7ab3d38922f
Harshitha sridhar <115911183+harshithaasridhar@users.noreply.github.com>
2023-07-17 15:46:14 +0530

    Add files via upload

---
+++ conditions2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class conditions2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a % b == 0){
+        System.out.println("The first number is divisible by the second");
+        }else{
+            System.out.println("Both numbers are not equal and none of them can be divided");
+
+    }
+    
+}
+}
