aba52cac65017a7e37aef07cbc3a778fc7b7a659
Harshitha sridhar <115911183+harshithaasridhar@users.noreply.github.com>
2023-07-17 15:20:24 +0530

    Add files via upload

---
+++ conditions1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class conditions1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a==b){
+            System.out.println("equal");
+            }else if (a>b){
+                System.out.println ("greater than");
+            }
+            else{
+                System.out.println ("lesser than or equal to ");
+            }
+        }
+    }
+    
+
